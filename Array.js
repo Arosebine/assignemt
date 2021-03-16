2a8bca2de3aaf17447d10343a6bc80b1c104c5a0
Arowolo Ebine <79418698+Arosebine@users.noreply.github.com>
2021-03-16 16:20:57 +0100

    Add files via upload

---
+++ Array.js
@@ -0,0 +1,50 @@
+
+let iQuestions = [
+    {
+        question1: ' God is a man of .....',
+        options: ['violent', 'fire', 'war', 'villain'],
+        answer: 0
+    },
+
+    {
+        question2: 'The Bible is written by who?',
+        options: ['God himself', 'The apostle', 'The Kings', 'Men of God'],
+        answer: 1
+    },
+
+    {
+        question3: 'How many books in the new testament?',
+        options: ['62', '65', '66', '68'],
+        answer: 2
+    },
+
+    {
+        question4: 'The Bible is divided into _____',
+        options: ['new and old bible', 'logo and rhelma', 'kingdom and believer', 'new and old testament'],
+        answer: 3
+    },
+
+    {
+        question5: 'which book is alternative to the bible?',
+        options: ['quran', 'tera', 'literature', 'none'],
+        answer: 4
+    },
+
+
+    {
+        question6: 'According to the John 6 vs 63, the bible is a ______',
+        options: ['living book', 'vision book', 'study book', 'merciful book'],
+        answer: 5
+    },
+
+
+    {
+        question7: 'Reading the bible help us to grow _______',
+        options: ['spiritually', 'in stature', 'educationally', 'strong'],
+        answer: 6
+    }
+
+  
+
+]
+console.log(iQuestions);
